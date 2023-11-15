23f46cb71de537fc3a9d82c23eb361f1efb2748b
Isaque <isaquebarbosa@outlook.com>
2023-11-15 19:56:46 -0300

    primeiro commit

---
+++ AloMundo.java
@@ -0,0 +1,5 @@
+class AloMundo {
+	public static void main(String[] args){
+		System.out.println("fernando eh maluco, filho do bluezao com nando moura");
+	}
+}
---
+++ AloMundoLegal.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+class AloMundoLegal {
+	public static void main(String[] args){
+		JOptionPane.showMessageDialog(null,"alo mundo");
+	}
+}
